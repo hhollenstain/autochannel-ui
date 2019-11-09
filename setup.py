e6db0f20e0ddc31225bf227cc77318397046369a
Henry Hollenstain <henry.l.hollenstain@gmail.com>
2019-11-09 11:34:13 -0800

    2.2.0 Updating channel size limits from 1-99

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ TEST_REQUIREMENTS = {
 
 setup(
     name='AutoChannel-ui',
-    version='2.1.0',
+    version='2.2.0',
     description='AutoChannel Discord Bot API',
     url='https://github.com/hhollenstain/autochannel-ui',
     packages=find_packages(),
